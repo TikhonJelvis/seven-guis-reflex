2fec316a6e390c503381eb803c6546f742f1fbc6
Tikhon Jelvis <tikhon@jelv.is>
2022-05-01 12:17:53 -0700

    Initial project + flake setup

---
+++ apps/Main.hs
@@ -0,0 +1,4 @@
+module Main where
+
+main :: IO ()
+main = putStrLn "This will eventually let you run any of the seven example GUI tasks."
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "An implementation of the 'Seven GUIs' benchmark using reflex-frp.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskellPackages;
+
+        package = returnShellEnv:
+          haskellPackages.developPackage {
+            inherit returnShellEnv;
+            name = "seven-guis";
+            root = ./.;
+
+            source-overrides = {
+              text-display = "0.0.2.0";
+            };
+
+            overrides = self: super: with haskellPackages.lib; {
+            };
+
+            modifier = drv:
+              pkgs.haskell.lib.addBuildTools drv (with haskellPackages; [
+                cabal-install
+                ghcid
+                haskell-language-server
+
+                cabal-fmt
+                stylish-haskell
+                pkgs.nixpkgs-fmt
+              ]);
+          };
+      in {
+        defaultPackage = package false;
+        devShell = package true;
+      });
+}
