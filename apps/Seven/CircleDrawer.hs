84c9ea6bc1f74b37ffc44c1ccfa9a8495614e865
Tikhon Jelvis <tikhon@jelv.is>
2022-05-22 22:53:19 -0700

    Cleaned up some of the circle-drawer code

--- apps/Seven/CircleDrawer.hs
+++ apps/Seven/CircleDrawer.hs
@@ -11,7 +11,6 @@
 {-# LANGUAGE RecursiveDo           #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TupleSections         #-}
-{-# LANGUAGE TypeApplications      #-}
 module Seven.CircleDrawer where
 
 import           Seven.Attributes    (ToAttributes (..))
@@ -46,6 +45,11 @@ import qualified Text.Printf         as Text
 
 import           Witherable          (Filterable (..), catMaybes, (<&?>))
 
+main :: IO ()
+main = do
+  css <- BS.readFile "css/tasks.css"
+  Dom.mainWidgetWithCss css widget
+
 widget :: forall m t. Dom t m => m ()
 widget = Dom.elClass "div" "circle-drawer" do
   action <- Dom.elClass "div" "centered controls" do
@@ -53,29 +57,24 @@ widget = Dom.elClass "div" "circle-drawer" do
     redos <- Dom.button "↷"
     pure $ leftmost [undos, redos]
 
-  Dom.elClass "div" "canvas" do
-    rec circles <- foldDyn applyAction mempty $
-          leftmost [Just <$> adds, previews, modifies]
+  rec (canvas, clicked) <- circlesCanvas circles beingModified
+      circles <- foldDyn applyAction mempty $
+        leftmost [Just <$> adds, previews, modifies]
 
-        let adds = mainClicks canvas <&> \ MouseEventResult { offset = (x, y) } ->
-              AddCircle (fromIntegral x, fromIntegral y)
-            modifies =
-              changeRadius <$> current modifiedCircle <@> setRadius
-            previews =
-              changeRadius <$> current modifiedCircle <@> updated previewRadius
+      let adds = mainClicks canvas <&> \ MouseEventResult { offset = (x, y) } ->
+            AddCircle (fromIntegral x, fromIntegral y)
+          modifies =
+            changeRadius <$> current modifiedCircle <@> setRadius
+          previews =
+            changeRadius <$> current modifiedCircle <@> updated previewRadius
 
-        let highlighted = zipDynWith (<|>) beingModified hovered
-        (canvas, (hovered, clicked)) <- circlesCanvas circles highlighted
+      beingModified <- holdDyn Nothing $
+        leftmost [Just <$> clicked, Nothing <$ setRadius]
 
-        beingModified <- holdDyn Nothing $
-          leftmost [Just <$> clicked, Nothing <$ setRadius]
-
-        let modifiedCircle = zipDynWith getCircle beingModified circles
-        (setRadius, previewRadius) <-
-          circleDialog beingModified $ fmap snd <$> modifiedCircle
-
-    output beingModified
-    pure ()
+      let modifiedCircle = zipDynWith getCircle beingModified circles
+      (setRadius, previewRadius) <-
+        circleDialog beingModified $ fmap snd <$> modifiedCircle
+  pure ()
   where changeRadius (Just (i, Circle { radius })) newRadius =
           Just $ ChangeRadius i Diff { before = radius, after = newRadius }
         changeRadius Nothing _ = Nothing
@@ -88,97 +87,35 @@ widget = Dom.elClass "div" "circle-drawer" do
 
 -- | The SVG element where the circles are rendered.
 --
--- Returns three values:
+-- Returns:
 --
---  * the containing SVG element itself
---  * a 'Dynamic' specifying which circle, if any, has the mouse over
---    it
---  * an 'Event' that fires when a circle is clicked
+--  * the containing SVG element
+--  * an 'Event' that fires when a circle is middle clicked
 circlesCanvas :: forall m t. Dom t m
               => Dynamic t (PushMap Circle)
               -- ^ The full set of circles to render.
               -> Dynamic t (Maybe Int)
               -- ^ Which circle, if any, should be highlighted.
               -> m ( Dom.Element EventResult Dom.GhcjsDomSpace t
-                   , (Dynamic t (Maybe Int), Event t Int) )
+                   , Event t Int )
 circlesCanvas circles highlighted = svg' "svg" do
-  let justAdded = updated $ PushMap.maxKey <$> circles
-  (hoveredSvg, clicked) <-
-    fanCircle <$> selectView highlighted circles (withId svgCircle)
-
-  hovered <- holdDyn Nothing $ leftmost [justAdded, hoveredSvg]
-  pure (hovered, clicked)
-  where fanCircle events =
-          ( events <&?> \case
-              (Over, i)  -> Just (Just i)
-              (Out, _)   -> Just Nothing
-              (Click, _) -> Nothing
-          , events <&?> \case
-              (Click, i) -> Just i
-              _          -> Nothing
-          )
-
-
--- | Editing actions we can take in the UI
-data Action = AddCircle (Double, Double)
-            -- ^ Add a circle with the standard radius at the given
-            -- point.
-            | ChangeRadius Int (Diff Double)
-            --              ↑        ↑
-            --             id      radius
-            -- ^ Change the radius of the circle with the given id.
-  deriving stock (Show, Eq)
-  deriving Display via (ShowInstance Action)
-
--- | Execute the action, if possible, on the given state.
---
--- Some actions like a 'ChangeRadius' for an id that is not in the map
--- cannot be executed, in which case the input map is returned
--- unchanged.
-applyAction :: Maybe Action -> PushMap Circle -> PushMap Circle
-applyAction Nothing circles = circles
-applyAction (Just (AddCircle center)) circles =
-  PushMap.push Circle { center, radius = 50 } circles
-applyAction (Just (ChangeRadius i radius)) circles =
-  case PushMap.lookup i circles of
-    Just c  -> PushMap.insert i c { radius = after radius } circles
-    Nothing -> circles
-
-
--- | An atomic change to a value, recording the value before and after
--- the change.
-data Diff a = Diff
-  { before :: a
-  , after  :: a
-  }
-  deriving stock (Show, Eq)
+  rawClicks <- selectView highlighted circles (withId svgCircle)
+  let clicks = rawClicks <&?> \ (MouseEventResult { button }, i) ->
+        [i | button == Auxiliary]
+  pure clicks
 
 -- | Render an SVG circle.
 --
--- The 'Event' returned will trigger when the mouse is moved over the
--- element or the element is clicked.
+-- Returns an 'Event' that triggers each time the circle is clicked.
 svgCircle :: forall m t. Dom t m
           => Dynamic t Circle
           -> Dynamic t Bool
-          -> m (Event t CircleEvent)
+          -> m (Event t MouseEventResult)
 svgCircle c isSelected = do
-  element <- circle c $ withDefaults <$> fillSelect
-  isHovered <- hovering True element
-  let hover = updated $ isHovered <&> bool Out Over
-      isAuxiliary e = button e == Auxiliary
-      click = Click <$ Witherable.filter isAuxiliary (Dom.domEvent Dom.Click element)
-  pure $ leftmost [click, hover]
-  where fillSelect = bool [("fill", "#fff0")] [("fill", "gray")] <$> isSelected
-        withDefaults = (<> toAttributes def { width = 2 })
-
--- | Events to track on circles generated by the user.
-data CircleEvent = Over
-                 -- ^ The mouse moved over the circle
-                 | Out
-                 -- ^ The mouse moved out of the circle
-                 | Click
-                 -- ^ The user clicked on the circle
-  deriving stock (Show, Eq, Ord, Enum, Bounded)
+  element <- circle c $ class_ <*> constDyn defaults
+  pure $ Dom.domEvent Dom.Click element
+  where class_ = setClass "selected" <$> isSelected
+        defaults = toAttributes def { width = 2 }
 
 -- | The dialog that lets us control the radius of a cricle.
 --
@@ -215,8 +152,37 @@ circleDialog beingModified targetCircle = do
 
         blank = Circle { center = (0, 0), radius = 0 }
 
+-- * Editing Actions
 
-main :: IO ()
-main = do
-  css <- BS.readFile "css/tasks.css"
-  Dom.mainWidgetWithCss css widget
+-- | Editing actions we can take in the UI
+data Action = AddCircle (Double, Double)
+            -- ^ Add a circle with the standard radius at the given
+            -- point.
+            | ChangeRadius Int (Diff Double)
+            --              ↑        ↑
+            --             id      radius
+            -- ^ Change the radius of the circle with the given id.
+  deriving stock (Show, Eq)
+  deriving Display via (ShowInstance Action)
+
+-- | Execute the action, if possible, on the given state.
+--
+-- Some actions like a 'ChangeRadius' for an id that is not in the map
+-- cannot be executed, in which case the input map is returned
+-- unchanged.
+applyAction :: Maybe Action -> PushMap Circle -> PushMap Circle
+applyAction Nothing circles = circles
+applyAction (Just (AddCircle center)) circles =
+  PushMap.push Circle { center, radius = 50 } circles
+applyAction (Just (ChangeRadius i radius)) circles =
+  case PushMap.lookup i circles of
+    Just c  -> PushMap.insert i c { radius = after radius } circles
+    Nothing -> circles
+
+-- | An atomic change to a value, recording the value before and after
+-- the change.
+data Diff a = Diff
+  { before :: a
+  , after  :: a
+  }
+  deriving stock (Show, Eq)
--- apps/Seven/Widget.hs
+++ apps/Seven/Widget.hs
@@ -32,6 +32,8 @@ import           Data.IntSet       (IntSet)
 import           Data.Map          (Map)
 import qualified Data.Map          as Map
 import           Data.Maybe        (fromMaybe, listToMaybe)
+import           Data.Set          (Set)
+import qualified Data.Set          as Set
 import           Data.Text         (Text)
 import qualified Data.Text         as Text
 import           Data.Text.Display (Display)
@@ -51,10 +53,155 @@ import           Text.Read         (readMaybe)
 
 -- ** Attributes
 
--- | Add the given class to the set of attributes. If the class is
--- already present, this will add it again.
-withClass :: Text -> Map Text Text -> Map Text Text
-withClass = Map.insertWith (\ a b -> a <> " " <> b) "class"
+-- *** CSS Classes
+
+-- | Does the given set of attributes have a class with the given
+-- name?
+--
+-- >>> hasClass "selected" [("class", "my selected widget")]
+-- True
+--
+-- >>> hasClass "selected" [("class", "my widget")]
+-- False
+--
+-- >>> hasClass "selected" []
+-- False
+hasClass :: Text -> Map Text Text -> Bool
+hasClass class_ attributes = case Map.lookup "class" attributes of
+  Just attribute -> class_ `elem` classes attribute
+  Nothing        -> False
+
+-- | Add a class to the given set of attributes.
+--
+-- This function will always normalize existing class attribute by
+-- dropping extra whitespace, sorting the class names and dropping any
+-- duplicates.
+--
+-- >>> addClass "selected" []
+-- fromList [("class","selected")]
+--
+-- >>> addClass "selected" [("class","selected   widget   selected")]
+-- fromList [("class","selected widget")]
+--
+-- >>> addClass "selected" [("class", "my widget")]
+-- fromList [("class","my selected widget")]
+--
+addClass :: Text -> Map Text Text -> Map Text Text
+addClass = Map.insertWith add "class"
+  where add class_ = joinClasses . Set.insert class_ . classes
+
+-- | Add or remove a class based on a boolean: 'True' adds the class,
+-- 'False' removes it.
+--
+-- >>> setClass "selected" True [("class", "my widget")]
+-- fromList [("class","my selected widget")]
+--
+-- >>> setClass "selected" False [("class", "my selected widget")]
+-- fromList [("class","my widget")]
+--
+setClass :: Text -> Bool -> Map Text Text -> Map Text Text
+setClass class_ = bool (removeClass class_) (addClass class_)
+
+-- | Remove the class from the given set of attributes if it is
+-- present.
+--
+-- This function will always normalize existing class attribute by
+-- dropping extra whitespace, sorting the class names and dropping any
+-- duplicates.
+--
+-- If the resulting class attribute would be empty, the attribute is
+-- removed from the set of attributes altogether.
+--
+-- >>> removeClass "selected" [("class", "my selected widget")]
+-- fromList [("class","my widget")]
+--
+-- >>> removeClass "selected" [("class", "my widget")]
+-- fromList [("class","my widget")]
+--
+-- >>> removeClass "selected" [("class","")]
+-- fromList []
+--
+-- >>> removeClass "selected" []
+-- fromList []
+--
+removeClass :: Text -> Map Text Text -> Map Text Text
+removeClass class_ = Map.update remove "class"
+  where remove attribute =
+          case joinClasses $ Set.delete class_ $ classes attribute of
+            ""  -> Nothing
+            new -> Just new
+
+-- | Given an entry for a @"class"@ attribute, parse out the classes
+-- set by the attribute.
+--
+-- >>> classes "foo"
+-- fromList ["foo"]
+--
+-- >>> classes "foo bar baz"
+-- fromList ["bar","baz","foo"]
+--
+-- >>> classes "  foo   bar  "
+-- fromList ["bar","foo"]
+--
+-- Note that classes can have non-ASCII whitespace as part of the
+-- name:
+--
+-- >>> classes "bar  foo   "
+-- fromList ["bar\8239\8239foo","\8239"]
+--
+classes :: Text
+        -- ^ Class attribute
+        -> Set Text
+classes = Set.delete "" . Set.fromList . Text.split isHtmlWhitespace
+
+-- | Combine a set of classes into a single attribute value, with each
+-- class name separated by a single space.
+--
+-- >>> joinClasses (Set.fromList ["widget", "my", "selected"])
+-- "my selected widget"
+--
+-- >>> joinClasses []
+-- ""
+--
+joinClasses :: Foldable f => f Text -> Text
+joinClasses = Text.intercalate " " . Foldable.toList
+
+-- | Is the character an HTML whitespace character?
+--
+-- HTML uses ASCII whitespace characters to separate class names/etc,
+-- but does not treat non-ASCII whitespace specially in those cases.
+--
+-- See [the definition of ASCII
+-- whitespace](https://infra.spec.whatwg.org/#ascii-whitespace) for
+-- details.
+--
+-- >>> isHtmlWhitespace ' '
+-- True
+--
+-- >>> isHtmlWhitespace 'a'
+-- False
+--
+-- >>> isHtmlWhitespace '\8239' -- narrow no-break space
+-- False
+--
+isHtmlWhitespace :: Char -> Bool
+isHtmlWhitespace c = c `elem` htmlWhitespace
+
+-- | The set of ASCII whitespace characters as defined by the HTML
+-- standard.
+--
+-- HTML uses ASCII whitespace characters to separate class names/etc,
+-- but does not treat non-ASCII whitespace specially in those cases.
+--
+-- See [the definition of ASCII
+-- whitespace](https://infra.spec.whatwg.org/#ascii-whitespace) for
+-- details.
+--
+-- >>> htmlWhitespace
+-- fromList "\t\n\f\r "
+--
+htmlWhitespace :: Set Char
+htmlWhitespace = [' ', '\t', '\n', '\f', '\r']
 
 -- ** Outputs
 
@@ -78,7 +225,7 @@ output' :: forall a m t. (Display a, Dom t m)
         -> Dynamic t a
         -- ^ Dynamically changing value to display.
         -> m ()
-output' attributes value = elDynAttr "div" (withClass "output" <$> attributes) do
+output' attributes value = elDynAttr "div" (addClass "output" <$> attributes) do
   dynText $ Display.display <$> value
 
 -- | A progress bar that fills up left-to-right.
@@ -90,7 +237,7 @@ progressBar :: forall m t. Dom t m
             -- 0, values > 1 are treated as 1.
             -> m ()
 progressBar progress = elClass "div" "progress-bar" do
-  elDynAttr "div" (withClass "bar" . toWidth <$> progress) $ pure ()
+  elDynAttr "div" (addClass "bar" . toWidth <$> progress) $ pure ()
   where toWidth :: Double -> Map Text Text
         toWidth (toPercent -> p) =
           [("style", Text.pack $ printf "width: %.2f%%" p)]
