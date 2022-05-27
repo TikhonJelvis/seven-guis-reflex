f504bab97fc3d53ec490ebc6cb85aeac90134ad3
Tikhon Jelvis <tikhon@jelv.is>
2022-05-26 17:17:49 -0700

    Only drag with `Main` mouse button

--- apps/UI/DragAndDrop.hs
+++ apps/UI/DragAndDrop.hs
@@ -29,11 +29,13 @@ import qualified Text.Printf                 as Text
 import           UI.Attributes               (addClass, setClass, setProperty)
 import           UI.Element                  (Dom, elClass', elDynAttr',
                                               offsetPosition)
-import           UI.Event                    (EventResult, addEventListener,
-                                              client, movement, offset)
+import           UI.Event                    (EventResult, MouseButton (..),
+                                              button, client, movement, offset)
 import qualified UI.Point                    as Point
 import           UI.Point                    (Point (..))
 import           UI.Widget                   (output)
+
+import qualified Witherable
 
 widget :: forall m t. Dom t m => m ()
 widget = do
@@ -51,10 +53,12 @@ widget = do
 -- Ideally, the container should be the parent of the draggable
 -- element. You can do this using @RecursiveDo@:
 --
+-- @
 -- widget = do
 --   rec (container, _) <- elClass' "div" "container" $
 --     draggable container "div" (constDyn []) (pure ())
 --   pure ()
+-- @
 draggable :: forall a m t. Dom t m
           => Dom.Element EventResult (Dom.DomBuilderSpace m) t
           -- ^ The element will only be draggable while the mouse is
@@ -70,7 +74,8 @@ draggable container tag attributes body = do
   rec (element, result) <- elDynAttr' tag attributes' body
       let attributes' = setClasses <$> held <*> (setPosition <$> position <*> attributes)
 
-      let mousedown = Dom.domEvent Dom.Mousedown element
+      let mousedown = Witherable.filter (\ e -> button e == Main) $
+            Dom.domEvent Dom.Mousedown element
           mouseup   = Dom.domEvent Dom.Mouseup container
           mousemove = Dom.domEvent Dom.Mousemove container
 
@@ -78,8 +83,7 @@ draggable container tag attributes body = do
         leftmost [False <$ mouseup, True <$ mousedown]
 
       -- the element and cursor position when drag starts
-      elementStart <- holdDyn (Point 0 0) $ traceEvent "elementPosition" $
-        elementPosition <$> mousedown
+      elementStart <- holdDyn (Point 0 0) $ elementPosition <$> mousedown
       dragStart    <- holdDyn (Point 0 0) $ client <$> mousedown
 
       let moves = attachWith (-) (current dragStart) $ client <$> mousemove
