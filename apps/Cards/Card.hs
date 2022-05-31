26c0f936fb7be1dcc54b0f6ff8bb4cb2ab9e353b
Tikhon Jelvis <tikhon@jelv.is>
2022-05-31 11:25:31 -0700

    Moved card code to this project temporarily
    Having the code in one place will make it far easier to simultaneously develop the card game library itself /and/ the supporting UI toolkit

---
+++ apps/Cards/Card.hs
@@ -0,0 +1,198 @@
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE NamedFieldPuns     #-}
+{-# LANGUAGE OverloadedStrings  #-}
+-- | "Standard" or "French suited" 52-card deck playing cards.
+--
+-- The standard 52-card deck has four __suits__:
+--
+--  * spades (♠️)
+--  * hearts (♥️)
+--  * diamonds (♦️)
+--  * clubs (♣️)
+--
+-- Each suit has 13 cards of different __ranks__:
+--
+--  * ace (A)
+--  * numeric cards (2–10)
+--  * jack (J)
+--  * queen (Q)
+--  * king (K)
+--
+module Cards.Card where
+
+import qualified Data.Char              as Char
+import           Data.Hashable          (Hashable)
+import           Data.Text              (Text)
+import           Data.Text.Display      (Display (displayBuilder))
+import qualified Data.Text.Lazy.Builder as Text
+import           Data.Vector            ((!))
+import qualified Data.Vector            as Vector
+
+import           GHC.Generics           (Generic)
+
+-- Doctest configuration (extensions/etc)
+-- $
+-- >>> :set -XOverloadedStrings
+
+-- | The four suits that make up a standard 52-card deck:
+--
+--  * spades (♠️)
+--  * hearts (♥️)
+--  * diamonds (♦️)
+--  * clubs (♣️)
+--
+-- Suits are ordered based on bridge conventions: ♠️ > ♥️ > ♦️ > ♣️, which
+-- is the same as alphabetical order based on their English names.
+--
+-- >>> Spades > Clubs
+-- True
+data Suit = Clubs | Diamonds | Hearts | Spades
+  deriving stock (Generic, Show, Eq, Ord, Enum, Bounded)
+  deriving anyclass (Hashable)
+
+-- | Colored symbols: ♠️, ♥️, ♦️, ♣️
+instance Display Suit where
+  displayBuilder = Text.fromText . suitShorthand
+
+-- | The Unicode representation of each suit symbol /in color/.
+--
+--  * 'Spades': @♠️@
+--  * 'Hearts': @♥️@
+--  * 'Diamonds': @♦️@
+--  * 'Clubs': @♣️@
+--
+-- Each symbol is a combination of two Unicode code points: the suit
+-- character (@♠@, @♥@, @♢@ and @♣@) followed by @VARIATION
+-- SELECTOR-16@ (#xfe0f) to make the symbol black or red as
+-- appropriate.
+--
+-- >>> suitShorthand Hearts == "♥️"
+-- True
+suitShorthand :: Suit -> Text
+suitShorthand Clubs    = "♣️"
+suitShorthand Diamonds = "♦️"
+suitShorthand Hearts   = "♥️"
+suitShorthand Spades   = "♠️"
+
+-- | Each suit has 13 cards with different __ranks__:
+--
+--  * ace (A)
+--  * numeric cards (2–10)
+--  * jack (J)
+--  * queen (Q)
+--  * king (K)
+--
+-- Ranks are ordered A < 2 < 3 < ... < 10 < J < Q < K
+--
+-- While the ordering for other cards is consistent in most conetexts,
+-- some games have "aces high" (2 < ... < K < A) or let aces be /both/
+-- high /and/ low (A < 2 ... < K < A). This makes the default ordering
+-- choice for 'Rank' somewhat arbitrary, but having an 'Ord' instance
+-- is useful enough to justify that.
+--
+-- >>> Ace < King
+-- True
+data Rank = Ace
+          | Two
+          | Three
+          | Four
+          | Five
+          | Six
+          | Seven
+          | Eight
+          | Nine
+          | Ten
+          | Jack
+          | Queen
+          | King
+  deriving stock (Generic, Show, Eq, Ord, Enum, Bounded)
+  deriving anyclass (Hashable)
+
+-- | Conventional shorthand: A, 2–10, J, K, Q
+instance Display Rank where
+  displayBuilder = Text.fromText . rankShorthand
+
+-- | The conventional shorthand for each rank:
+--
+--  * ace: @"A"@
+--  * 2–10: @"2"@–@"10"@
+--  * jack: @"J"@
+--  * queen: @"Q"@
+--  * king: @"K"@
+rankShorthand :: Rank -> Text
+rankShorthand Ace   = "A"
+rankShorthand Two   = "2"
+rankShorthand Three = "3"
+rankShorthand Four  = "4"
+rankShorthand Five  = "5"
+rankShorthand Six   = "6"
+rankShorthand Seven = "7"
+rankShorthand Eight = "8"
+rankShorthand Nine  = "9"
+rankShorthand Ten   = "10"
+rankShorthand Jack  = "J"
+rankShorthand Queen = "Q"
+rankShorthand King  = "K"
+
+-- | One of the 52 cards in a standard deck, covering each combination
+-- of rank and suit.
+data Card = Card
+  { rank :: !Rank
+  , suit :: !Suit
+  }
+  deriving stock (Generic, Show, Eq, Ord, Bounded)
+  deriving anyclass (Hashable)
+
+-- | Standard shorthand, eg A♠️ for the ace of spades
+instance Display Card where
+  displayBuilder Card { rank, suit } =
+    displayBuilder rank <> displayBuilder suit
+
+-- | The standard shorthand for a card: the rank followed by the suit
+-- (eg A♠️ for the ace of spades).
+--
+-- See also: 'rankShorthand' and 'suitShorthand'
+--
+-- >>> cardShorthand (Card Ace Spades) == "A♠️"
+-- True
+--
+-- >>> cardShorthand (Card Ten Clubs) == "10♣️"
+-- True
+--
+-- >>> cardShorthand (Card King Hearts) == "K♥️"
+-- True
+cardShorthand :: Card -> Text
+cardShorthand Card { rank, suit } = rankShorthand rank <> suitShorthand suit
+
+-- | The Unicode 7.0 character for each card (🂡, 🃊, 🃞... etc)
+--
+-- Note that these characters do not render well at smaller sizes in
+-- most fonts.
+--
+-- >>> cardCharacter (Card Ace Spades) == '🂡'
+-- True
+--
+-- >>> cardCharacter (Card Jack Diamonds) == '🃋'
+-- True
+--
+-- >>> cardCharacter (Card King Hearts) == '🂾'
+-- True
+cardCharacter :: Card -> Char
+cardCharacter Card { rank, suit } = case suit of
+  Clubs    -> clubs    ! fromEnum rank
+  Diamonds -> diamonds ! fromEnum rank
+  Hearts   -> hearts   ! fromEnum rank
+  Spades   -> spades   ! fromEnum rank
+  where clubs    = chars 0x1F0D1
+        diamonds = chars 0x1F0C1
+        hearts   = chars 0x1F0B1
+        spades   = chars 0x1F0A1
+
+        -- starting from code point for the ace of each suit
+        chars ace = Vector.fromList
+          [ Char.chr x
+          | x <- [ace .. ace + 13]
+          , x `mod` 16 /= 0xC -- skip "knight" card
+          ]
---
+++ apps/Cards/Place.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DerivingStrategies #-}
+-- | Many games involve placing cards face-up or face-down on a
+-- playing area, or putting cards in a pile.
+module Cards.Place where
+
+import           Data.Hashable         (Hashable)
+import           Data.Vector           (Vector)
+import           Data.Vector.Instances ()
+
+import           GHC.Generics          (Generic)
+
+-- | Whether a card is placed face-up or face-down.
+data Face = FaceUp
+          -- ^ A card placed face-up means that its rank and suit are
+          -- known to everybody.
+          | FaceDown
+          -- ^ A card placed face-down means that its rank and suit is
+          -- /not/ automatically known to everybody.
+  deriving stock (Show, Eq, Ord, Enum, Bounded, Generic)
+  deriving anyclass (Hashable)
+
+-- | A card placed on the playing area, either face-up or face-down.
+data Place a = Place
+  { status :: Face
+  , card   :: a
+  }
+  deriving stock (Show, Eq, Generic)
+  deriving anyclass (Hashable)
+
+-- | A pile of cards placed on on top of the other.
+newtype Pile a = Pile (Vector a)
+  deriving stock (Show, Eq, Generic)
+  deriving anyclass (Hashable)
