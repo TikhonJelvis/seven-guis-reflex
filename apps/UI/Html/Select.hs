9163c6e2a1e3906ed242c7a2d3db03913ce08fa9
Tikhon Jelvis <tikhon@jelv.is>
2022-06-23 09:55:38 -0700

    Reorganized select code a bit

--- apps/UI/Html/Select.hs
+++ apps/UI/Html/Select.hs
@@ -4,12 +4,178 @@ module UI.Html.Select
   , select
 
     -- ** Options
-  , Entry
+  , Entry (..)
   , disabled
+  , selected
   , option
+  , option'
   , placeholder
   , group
   )
 where
 
+import           Data.Maybe              (fromMaybe)
+import           Data.Text               (Text)
+import           Data.Text.Display       (Display, display)
+
+import qualified Reflex
+
+import           UI.Attributes           (AsAttributeValue)
+import qualified UI.Element              as Element
+import qualified UI.Html                 as Html
+import qualified UI.Html.Input           as Input
 import           UI.Html.Select.Internal
+import           UI.Main                 (Runnable (Runnable), withCss)
+
+
+-- | Make an option or group disabled by default.
+--
+-- __Example__
+--
+-- A 'select' with a mix of enabled and disabled options and groups:
+--
+-- @
+-- select [] Nothing never
+--   [ placeholder "--please select an ingredient--"
+--   , group "Fruit"
+--     [ option "Apple"
+--     , option "Pear"
+--     , disabled (option "Banana")
+--     ]
+--   , disabled $ group "Vegetables"
+--     [ disabled (option "Cucumber")
+--     , option "Squash" -- disabled since group is disabled
+--     ]
+--   ]
+-- @
+disabled :: forall k. Entry k -> Entry k
+disabled = \case
+  Option _ isSelected label key -> Option Input.Disabled isSelected label key
+  Group _ label options         -> Group Input.Disabled label options
+
+-- | Make an option selected by default.
+--
+-- Does not do anything if applied to a group.
+--
+-- Note: the initially selected value is currently /not/ reflected in
+-- the 'Dynamic' returned from 'select'. Instead, the initial value
+-- passed to 'select' is used until some event changes the value. This
+-- is a limitation of reflex-dom's 'Dom.selectElement' function so it
+-- will hopefully change in the future.
+--
+-- __Example__
+--
+-- A 'select' with the @"Apple"@ option selected by default:
+--
+-- @
+-- select [] (Just "Apple") never
+--   [ selected (option "Apple")
+--   , option "Pear"
+--   ]
+-- @
+selected :: forall k. Entry k -> Entry k
+selected = \case
+  Option enabled _ label key -> Option enabled True label key
+  g@Group{}                  -> g
+
+-- | An option whose user-facing label is the 'display' of its key.
+--
+-- __Examples__
+--
+-- An option labeled @true@:
+--
+-- @
+-- option "true"
+-- @
+--
+-- An option labeled @false@ and disabled:
+--
+-- @
+-- disabled (option "false")
+-- @
+option :: forall k. (AsAttributeValue k, Display k) => k -> Entry k
+option key = Option Input.Enabled False (Just key) (display key)
+
+-- | An option with the given key and user-facing label.
+--
+-- __Example__
+--
+-- An select with two options labeled "yes" or "no", mapping to 'True'
+-- and 'False' respectively:
+--
+-- @
+-- select [] (Just "yes") never
+--   [ selected (option' True "yes")
+--   , option' False "no"
+--   ]
+-- @
+option' :: forall k. AsAttributeValue k => k -> Text -> Entry k
+option' key = Option Input.Enabled False (Just key)
+
+-- | An option with @value=""@, useful for placeholders that do not
+-- correspond to a valid selection.
+--
+-- If the @select@ element has @required@ set, a 'placeholder' option
+-- will not count as a selection.
+--
+-- __Examples__
+--
+-- A placeholder:
+--
+-- @
+-- placeholder "--please select an option--"
+-- @
+placeholder :: forall k. Text -> Entry k
+placeholder = Option Input.Enabled False Nothing
+
+-- | A group of options (@optgroup@ element), enabled by default.
+--
+-- __Examples__
+--
+-- A group of options:
+--
+-- @
+-- group "Fruit"
+--   [ option "Apple"
+--   , option "Pear"
+--   ]
+-- @
+group :: forall k. Text -> [Entry k] -> Entry k
+group = Group Input.Enabled
+
+
+_demo :: IO ()
+_demo = do
+  withCss "css/ui.css" $ Runnable do
+    Html.div_ [] do
+      Html.label [] do
+        Element.text "No default:"
+        (_, selectedValue) <- select [] Nothing Reflex.never
+          [ placeholder "--please select an ingredient--"
+          , group "Fruit"
+            [ option "Apple"
+            , option "Pear"
+            , disabled (option "Banana")
+            ]
+          , disabled $ group "Vegetables"
+            [ disabled (option "Cucumber")
+            , option "Squash"
+            ]
+          ]
+        Element.dynText (fromMaybe "<none>" <$> selectedValue)
+
+    Html.div_ [] do
+      Html.label [] do
+        Element.text "With default selected:"
+        (_, selectedValue) <- select [] (Just "Pear") Reflex.never
+          [ group "Fruit"
+            [ option "Apple"
+            , option "Pear"
+            , disabled (option "Banana")
+            ]
+          , disabled $ group "Vegetables"
+            [ disabled (option "Cucumber")
+            , option "Squash"
+            ]
+          ]
+        Element.dynText (fromMaybe "<none>" <$> selectedValue)
--- apps/UI/Html/Select/Internal.hs
+++ apps/UI/Html/Select/Internal.hs
@@ -9,15 +9,12 @@ import           Control.Monad                     (forM_)
 import           Control.Monad.State               (evalState, get, put)
 
 import           Data.Hashable                     (Hashable)
-import           Data.Maybe                        (fromMaybe)
 import           Data.Text                         (Text)
-import           Data.Text.Display                 (Display, display)
 
 import           GHC.Generics                      (Generic)
 
 import qualified GHCJS.DOM.Types                   as GHCJS
 
-import qualified Reflex
 import           Reflex                            (Dynamic, Event, Reflex)
 import qualified Reflex.Dom                        as Dom
 
@@ -33,8 +30,6 @@ import qualified UI.Html                           as Html
 import           UI.Html                           (Html)
 import qualified UI.Html.Input                     as Input
 import           UI.Html.Input                     (Enabled)
-import           UI.Main                           (Runnable (Runnable),
-                                                    withCss)
 
 -- * Select Elements
 
@@ -109,6 +104,9 @@ select attributes initial setValue options = do
               rest' <- go k rest
               pure (group' : rest')
             [] -> pure []
+
+        selected (Option enabled _ label key) = Option enabled True label key
+        selected g@Group{}                    = g
 {-# INLINABLE select #-}
 
     -- TODO: support select menus with multiple selection
@@ -166,109 +164,12 @@ data Entry k where
   deriving stock (Show, Eq, Ord, Generic)
   deriving anyclass (Hashable)
 
--- | Make an option or group disabled by default.
---
--- __Example__
---
--- A 'select' with a mix of enabled and disabled options and groups:
---
--- @
--- select [] Nothing never
---   [ placeholder "--please select an ingredient--"
---   , group "Fruit"
---     [ option "Apple"
---     , option "Pear"
---     , disabled (option "Banana")
---     ]
---   , disabled $ group "Vegetables"
---     [ disabled (option "Cucumber")
---     , option "Squash" -- disabled since group is disabled
---     ]
---   ]
--- @
-disabled :: forall k. Entry k -> Entry k
-disabled = \case
-  Option _ isSelected label key -> Option Input.Disabled isSelected label key
-  Group _ label options         -> Group Input.Disabled label options
-
--- | Make an option selected by default.
---
--- Does not do anything if applied to a group.
---
--- Note: the initially selected value is currently /not/ reflected in
--- the 'Dynamic' returned from 'select'. Instead, the initial value
--- passed to 'select' is used until some event changes the value. This
--- is a limitation of reflex-dom's 'Dom.selectElement' function so it
--- will hopefully change in the future.
---
--- __Example__
---
--- A 'select' with the @"Apple"@ option selected by default:
---
--- @
--- select [] (Just "Apple") never
---   [ selected (option "Apple")
---   , option "Pear"
---   ]
--- @
-selected :: forall k. Entry k -> Entry k
-selected = \case
-  Option enabled _ label key -> Option enabled True label key
-  g@Group{}                  -> g
-
--- | An option whose user-facing label is the 'display' of its key.
---
--- Passing 'Nothing' for the key means @value=""@ for the
--- option—useful for placeholder options.
---
--- __Examples__
---
--- An option labeled @true@:
---
--- @
--- option "true"
--- @
---
--- An option labeled @false@ and disabled:
---
--- @
--- disabled (option "false")
--- @
-option :: (AsAttributeValue k, Display k) => k -> Entry k
-option key = Option Input.Enabled False (Just key) (display key)
-
--- | An option with @value=""@, useful for placeholders that do not
--- correspond to a valid selection.
---
--- If the @select@ element has @required@ set, a 'placeholder' option
--- will not count as a selection.
---
--- __Examples__
---
--- A placeholder:
---
--- @
--- placeholder "--please select an option--"
--- @
-placeholder :: forall k. Text -> Entry k
-placeholder = Option Input.Enabled False Nothing
-
--- | A group of options (@optgroup@ element), enabled by default.
---
--- __Examples__
---
--- A group of options:
---
--- @
--- group "Fruit"
---   [ option "Apple"
---   , option "Pear"
---   ]
--- @
-group :: forall k. Text -> [Entry k] -> Entry k
-group = Group Input.Enabled
-
 -- | Create an @option@ or @optgroup@ element for the given 'Entry'.
+--
+-- Note that, strictly speaking, HTML does not allow nesting
+-- @optgroup@ elements. In my experience browsers still try to render
+-- the resulting select by flattening the groups, but the exact
+-- behavior is not specified and may vary browser-to-browser.
 createEntry :: forall k m t. (AsAttributeValue k, Dom t m)
             => Entry k
             -> m (Html t)
@@ -290,39 +191,4 @@ createEntry = \case
                 , Input.enabled                   =: enabled
                 ]
           fst <$> Html.html @"option" attributes (Element.text label)
-
-_demo :: IO ()
-_demo = do
-  withCss "css/ui.css" $ Runnable do
-    Html.div_ [] do
-      Html.label [] do
-        Element.text "No default:"
-        (_, selectedValue) <- select [] Nothing Reflex.never
-          [ placeholder "--please select an ingredient--"
-          , group "Fruit"
-            [ option "Apple"
-            , option "Pear"
-            , disabled (option "Banana")
-            ]
-          , disabled $ group "Vegetables"
-            [ disabled (option "Cucumber")
-            , option "Squash"
-            ]
-          ]
-        Element.dynText (fromMaybe "<none>" <$> selectedValue)
-
-    Html.div_ [] do
-      Html.label [] do
-        Element.text "With default selected:"
-        (_, selectedValue) <- select [] (Just "Pear") Reflex.never
-          [ group "Fruit"
-            [ option "Apple"
-            , option "Pear"
-            , disabled (option "Banana")
-            ]
-          , disabled $ group "Vegetables"
-            [ disabled (option "Cucumber")
-            , option "Squash"
-            ]
-          ]
-        Element.dynText (fromMaybe "<none>" <$> selectedValue)
+{-# INLINABLE createEntry #-}
